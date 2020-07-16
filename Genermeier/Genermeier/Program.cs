d95cbeb89aade66a88f8a781f739644e4a468677
Nerovia <nerovia@icloud.com>
2020-07-17 01:00:35 +0200

    print tree

--- Genermeier/Genermeier/Program.cs
+++ Genermeier/Genermeier/Program.cs
@@ -8,27 +8,18 @@ namespace Genermeier
 {
     class Program
     {
-        static void DrawFamily(Personmeier person)
-        {
-            Console.Write("O");
-            // If family is even 
-            for (int n = 0; n < person.partners.Length; n++)
-            {
-                if (person.partners.Length % 2 != 0 && person.partners.Length / 2 == n)
-                    Console.Write("─┬─");
-                else
-                    Console.Write("───");
-                Console.Write("O");
-            }
-        }
-
         static void Main(string[] args)
         {
             var person = new Personmeier();
             person.Sprout(5);
 
-            DrawFamily(person);
 
+            person.PrintTree();
+
+            while (true) ;
         }
+
+
+
     }
 }
